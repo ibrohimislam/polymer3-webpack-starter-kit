130861c479b408b610f12abb4d9f11f6260c824a
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2018-07-16 23:46:44 +0700

    Add web-component-tester sample

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 import {setRootPath, setPassiveTouchGestures} from '@polymer/polymer/lib/utils/settings';
 import {MyApp} from './shell/shell.component';
+import {MyView1} from './view1/view1.component';
+import {MyView2} from './view2/view2.component';
+import {MyView3} from './view3/view3.component';
 
 declare var window: any;
 
@@ -8,6 +11,9 @@ setPassiveTouchGestures(true);
 
 const elements = [
   MyApp,
+  MyView1,
+  MyView2,
+  MyView3,
   // Add your STATIC elements here
 ];
 
